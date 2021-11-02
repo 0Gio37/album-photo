614401b06de638d982371e19123c184fa060318b
_GioLeone <gmessanges@gmail.com>
2021-11-02 20:36:42 +0100

    design home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="HomePage")
+     * @Route("/", name="HomePage")
      */
     public function index(): Response
     {
