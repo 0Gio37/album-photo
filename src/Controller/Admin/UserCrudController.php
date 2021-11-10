b645b2f886401566ad2357a76aa7855ce060337d
_GioLeone <gmessanges@gmail.com>
2021-11-10 15:12:02 +0100

    implement easyadmin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use phpDocumentor\Reflection\Types\Array_;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            TextField::new('username', 'Pseudo'),
+            ArrayField::new('roles'),
+            TextField::new('password')
+        ];
+    }
+
+}
