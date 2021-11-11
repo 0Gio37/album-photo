ff9dfc47363368ac00a6b4dd1d3eadecffd109df
_GioLeone <gmessanges@gmail.com>
2021-11-11 20:26:56 +0100

    initialisation form (en cours)

--- src/Controller/AddPhotoController.php
+++ src/Controller/AddPhotoController.php
@@ -2,19 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\PhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class AddPhotoController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/add-photo", name="add_photo")
      */
-    public function index(): Response
+    public function addPhoto(Request $request): Response
     {
-        return $this->render('home/addPhoto.html.twig', [
-            'controller_name' => 'AddPhotoController',
+
+        $photo = new Photo();
+        $form = $this->createForm(PhotoType::class, $photo);
+        $currentUser = $this->security->getUser();
+        dd($currentUser->getId());
+
+        return $this->render(
+            'home/addPhoto.html.twig', [
+            'photoForm' => $form->createView(),
         ]);
     }
 }
