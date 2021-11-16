7736beb320b224886420fe98ae9d8ec1c37c1fdf
_GioLeone <gmessanges@gmail.com>
2021-11-16 15:14:41 +0100

    display photo by album ok

--- src/Controller/DisplayAllController.php
+++ src/Controller/DisplayAllController.php
@@ -11,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DisplayAllController extends AbstractController
 {
+    public $toggleBtn = false;
+
     /**
      * @Route("/display/all-album", name="displayAllAlbum")
      */
-    public function index(AlbumRepository $AlbumRepository, PhotoRepository $PhotoRepository, EntityManagerInterface $em): Response
+    public function displayAllAlbum(AlbumRepository $AlbumRepository, PhotoRepository $PhotoRepository): Response
     {
         $albumList = $AlbumRepository->findBy([], ['titre'=>'ASC']);
         $photoList = $PhotoRepository->findBy([], ['id'=>'ASC']);
@@ -23,6 +25,41 @@ class DisplayAllController extends AbstractController
         return $this->render('display/all-albums.html.twig', [
             'albumList' => $albumList,
             'photoList'=>$photoList,
+        ]);
+    }
+
+
+    /**
+     * @Route("/display/single-album-inversChrono/{titleAlbum}/{idAlbum}", name="displayOneAlbumAntiChrono")
+     */
+    public function displayOneAlbumAntiChrono(PhotoRepository $PhotoRepository, $idAlbum, $titleAlbum): Response
+    {
+        $this->toggleBtn =  true;
+
+        $listPhotoAlbum = $PhotoRepository->findBy(['album'=>$idAlbum], ['id'=>'DESC']);
+
+        return $this->render('display/single-album.html.twig', [
+            'idAlbum'=>$idAlbum,
+            'titleAlbum'=>$titleAlbum,
+            'listPhotoAlbum'=>$listPhotoAlbum,
+            'toggleBtn'=> $this->toggleBtn,
+        ]);
+    }
+
+    /**
+     * @Route("/display/single-album-chrono/{titleAlbum}/{idAlbum}", name="displayOneAlbumChrono")
+     */
+    public function displayOneAlbumChrono(PhotoRepository $PhotoRepository, $idAlbum, $titleAlbum): Response
+    {
+        $this->toggleBtn =  false;
+
+        $listPhotoAlbum = $PhotoRepository->findBy(['album'=>$idAlbum], ['id'=>'ASC']);
+
+        return $this->render('display/single-album.html.twig', [
+            'idAlbum'=>$idAlbum,
+            'titleAlbum'=>$titleAlbum,
+            'listPhotoAlbum'=>$listPhotoAlbum,
+            'toggleBtn'=> $this->toggleBtn,
         ]);
     }
 }
