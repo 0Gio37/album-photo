22e7760f95e565c7fd09428df6bed685fe3fadd3
_GioLeone <gmessanges@gmail.com>
2022-05-29 18:54:18 +0200

    fix hash pass

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -49,34 +51,27 @@ class SecurityController extends AbstractController
     /**
      * @Route("/sigin", name="sigin")
      */
-
-    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder):Response
+    public function new(Request $request, UserPasswordHasherInterface $passwordEncoder):Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
-
-
             $user = $form->getData();
 
             // Encoder le mot de passe
-            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
+            $user->setPassword($passwordEncoder->hashPassword($user, $user->getPassword()));
 
             //Ajout du role par défaut
             $user->setRoles(["ROLE_USER"]);
 
             //Ajout du statut de validation par défaut
            $user->setIsValid(false);
-
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('login');
-            }
+           $this->entityManager->persist($user);
+           $this->entityManager->flush();
+           return $this->redirectToRoute('login');
+        }
 
         return $this->render(
             'security/sign-in.html.twig', [
