ab55ecc2bbc180c4f655a6c37c37a539f8d7fb77
_GioLeone <gmessanges@gmail.com>
2022-06-06 20:02:17 +0200

    end fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     theme: {
         height: {
             hCustomMax50: 'max-height: 50%',
-
-}
-}
+        },
+        extend: {
+            colors: {
+                customColorMainBG : 'background-color: #161616',
+            },
+        }
+    }
 }
