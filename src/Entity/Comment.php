bf7f90905a003b89240201490c9bfa8a692a7843
_GioLeone <gmessanges@gmail.com>
2022-09-22 21:29:56 +0200

    fix comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -14,6 +14,40 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Comment
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="commentaires")
+     * @ORM\JoinColumn(onDelete = "CASCADE")
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $texte;
+
+    /**
+     * @ORM\Column(type="datetime" , options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createAt;
+
+//    /**
+//     * @ORM\OneToMany(targetEntity=LienCommentPhoto::class, mappedBy="comment")
+//     * @ORM\JoinColumn(onDelete = "CASCADE")
+//     */
+//    private $photo;
+
+    public function __construct()
+    {
+        $this->photo = new ArrayCollection();
+    }
+
 
 }
 
