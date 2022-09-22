b4b876ba20ea85e47e5d5ff4574d18de4807f505
_GioLeone <gmessanges@gmail.com>
2022-09-22 21:26:25 +0200

    fix comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -1,1 +1,19 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentsRepository;
+use App\Repository\CommentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Comment
+{
+
+}
 
