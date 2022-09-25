ee1146ba3a2d4036d8ebd96ead1d0c2cacf767d7
_GioLeone <gmessanges@gmail.com>
2022-09-25 15:59:47 +0200

    purge css in webpack config js

--- webpack.config.js
+++ webpack.config.js
@@ -75,5 +75,16 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 ;
+if (Encore.isProduction()) {
+    Encore.addPlugin(new PurgeCssPlugin({
+        paths: glob.sync([
+            path.join(__dirname, 'templates/**/*.html.twig')
+        ]),
+        defaultExtractor: (content) => {
+            return content.match(/[\w-/:]+(?<!:)/g) || [];
+        }
+    }));
+}
+;
 
 module.exports = Encore.getWebpackConfig();
