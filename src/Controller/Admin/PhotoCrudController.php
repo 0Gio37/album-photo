2ddc107599d277c02269fa3bf1f53821f8a0c006
_GioLeone <gmessanges@gmail.com>
2022-10-09 15:27:51 +0200

    test aesyadmin set datetime in prod

--- src/Controller/Admin/PhotoCrudController.php
+++ src/Controller/Admin/PhotoCrudController.php
@@ -27,7 +27,7 @@ class PhotoCrudController extends AbstractCrudController
         return [
             //DateField::new('createAt', 'Créé le')->hideOnForm(),
 
-            DateField::new('created_at', 'Créé le')->setFormat(DateTimeField::FORMAT_SHORT)->setFormat('dd/MM/yyyy')->setTimezone('Europe/London')->hideOnForm(),
+            DateField::new('created_at', 'Créé le')->setFormat(DateTimeField::FORMAT_SHORT)->setTimezone('Europe/London')->hideOnForm(),
 
             AssociationField::new('auteur', 'Par')->hideOnForm(),
             AssociationField::new('album', 'album' )->hideOnForm(),
